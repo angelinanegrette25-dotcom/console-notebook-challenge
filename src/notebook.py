3fc55866eb7c66f7129c7470bcf45b516a949962
Angelina <angelinanegrette25@gmail.com>
2026-02-16 17:52:59 -0500

    First class

--- src/notebook.py
+++ src/notebook.py
@@ -1,1 +1,17 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from detenime import detetime
+
+ class note:
+     high = "HIGH"
+     MEDIUM = "MEDIUM"
+     low = "LOW"
+
+ def __init__(self,code:int,title:str,text:str,importance:str):
+    self.code = code
+    self.title = title
+    self.text = text
+    self.importance = importance
+    self.creacion_date= detetime
+    self.tangs=
+
+
+
